2c097698ef510197a41a25d19d226561ad1bbc80
Zimgo012 <rjccabelcina@gmail.com>
2025-05-08 16:15:47 -0400

    feat: added database with docker/postgres, configure simple post and get request

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @ToString
@@ -8,11 +9,26 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE)
     private long id;
     private String name;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
-
+    public Student(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
 }
